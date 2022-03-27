e5004e2917b895171028bca38bc0daf1554d02d2
evememine <evememine@gmail.com>
2022-03-28 00:32:47 +0900

    test

---
+++ client/src/Layout/MyBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './MyBox.module.css';
+
+
+const Mybox = () => {
+  return (
+    <div className={classes.mybox}>Mybox</div>
+  )
+}
+
+export default Mybox
