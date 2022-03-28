a89769bda912a7fc812f22d7478a7b6e3232134d
Youngae KIM <mary900109@gmail.com>
2022-03-29 01:11:15 +0900

    [ADD] api

--- server/meditime/src/main/java/dev/medi/meditime/controller/MediInfoController.java
+++ server/meditime/src/main/java/dev/medi/meditime/controller/MediInfoController.java
@@ -1,5 +1,49 @@
 package dev.medi.meditime.controller;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin(origins= "*")
+@RestController
+@RequestMapping("api/v1/medisons")
 public class MediInfoController {
     
+    @GetMapping("/{id}")
+    public String getMadiInfo(@PathVariable String id) {
+        StringBuffer result = new StringBuffer();
+        try{
+            StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList"); /*URL*/
+            urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=inPV4SC7UpBskkz251GkNF8v52AGPJBKOakLrAmtMXBFyBDLTt9wUKo3J1IQMkNF1HAzfkfJQnUgM0HZQDMA2w%3D%3D"); /*Service Key*/
+            urlBuilder.append("&" + URLEncoder.encode("itemName","UTF-8") + "=" + URLEncoder.encode(id, "UTF-8")); /*제품명*/
+            urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*응답데이터 형식(xml/json) Default:xml*/
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+            // System.out.println("Response code: " + conn.getResponseCode());
+            BufferedReader rd;
+            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+            // System.out.println(sb.toString());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return result + "";
+    }
 }
