210610ab2eb56a0b8c021d05f67cf9daeacc12ec
Youngae KIM <mary900109@gmail.com>
2022-03-31 00:03:35 +0900

    [UPDATE] modified query

--- server/meditime/src/main/java/dev/medi/meditime/repository/MediRepository.java
+++ server/meditime/src/main/java/dev/medi/meditime/repository/MediRepository.java
@@ -3,9 +3,15 @@ package dev.medi.meditime.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import dev.medi.meditime.model.entity.Medi;
 
 public interface MediRepository extends JpaRepository<Medi, String>{
-    List<Medi> findByitemNameContainingOrEfcItemContaining(String itemName, String efcItem);
+    
+    // List<Medi> findByitemNameContainingOrEfcItemContaining(String itemName, String efcItem);
+
+    @Query("select m from Medi m where m.itemName like %:itemName% or m.efcItem like %:efcItem%")
+    List<Medi> searchMediList(@Param("itemName") String itemName, @Param("efcItem") String efcItem );
 }
