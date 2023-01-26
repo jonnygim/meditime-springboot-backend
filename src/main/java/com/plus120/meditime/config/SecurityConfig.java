074667304e2ced3ab3479ab0d8920f507552236a
jonnygim <jonnygim.dev@gmail.com>
2023-01-26 16:25:06 +0900

    feat : spring security 필터체인 추가

--- src/main/java/com/plus120/meditime/config/SecurityConfig.java
+++ src/main/java/com/plus120/meditime/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.plus120.meditime.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,4 +14,30 @@ public class SecurityConfig {
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                // CSRF 설정 Disable
+                .csrf().disable()
+
+                // h2-console 을 위한 설정을 추가
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 시큐리티는 기본적으로 세션을 사용
+                // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                // 로그인, 회원가입 API등은 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/users/**").permitAll()
+                .and()
+                .build();
+    }
+
 }
